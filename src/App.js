5860b696098e18ff7a550f679bc9fb318a5c2814
João Pedro <developerjotape@gmail.com>
2022-02-17 20:40:53 -0300

    TMDB, MovieRow add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default() =>{
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      //Pegando a lista do TMDB
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+    loadAll();
+  }, []);
+
   return(
-    <div>
-      Olá mundo
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item, key) =>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   )
 };
